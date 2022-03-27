3bf05d8815f4d41e6d8c43c7c6934aaa0b5cc25b
Daniel Alexander Kates <alex.kates@creditly.com>
2022-03-27 17:08:03 -0400

    styling

--- frontend/pages/feed.tsx
+++ frontend/pages/feed.tsx
@@ -47,82 +47,91 @@ const FeedResponse: NextPage = () => {
   }
 
   return (
-    <div className="flex-col flex items-center my-8">
-      {data?.feed
-        .filter((animal) => animal.photos.length > 0)
-        .filter((animal) => animal.contact.email)
-        .filter((animal) => animal.url)
-        .map((animal, i) => {
-          const tweetText = `Check out this ${animal.type.toLocaleLowerCase()} named ${
-            animal.name
-          } I found on Petsura!`;
-
-          const emailSubject = `I'm interested in adopting ${animal.name}!`;
-          const emailBody = `Hi, I came across ${animal.name} at ${animal.url} and I'm interested in adopting! Looking forward to hearing from you soon!`;
-          return (
-            <div
-              key={i}
-              className="flex flex-col items-center w-96 h-auto rounded-lg border border-gray-200 shadow-lg m-4"
-            >
-              <img
-                className="w-full h-96 object-cover"
-                src={animal.photos[0].full}
-                alt={animal.name}
-              />
-              <h2 className="my-4 text-3xl font-medium text-gray-900">
-                {animal.name}
-              </h2>
-              <div className="flex w-5/6 justify-evenly">
-                <span className="flex items-center text-sm dark:text-gray-600">
-                  <FaBirthdayCake />
-                  <span className="pl-2">{animal.age?.toLowerCase()}</span>
-                </span>
-                <span className="flex items-center text-sm dark:text-gray-600">
-                  <MdQueryStats />
-                  <span className="pl-2">{animal.status?.toLowerCase()}</span>
-                </span>
-                <span className="flex items-center text-sm dark:text-gray-600">
-                  <AiOutlineColumnWidth />
-                  <span className="pl-2">{animal.size?.toLowerCase()}</span>
-                </span>
-              </div>
-              <div className="flex justify-evenly w-5/6 my-4 space-x-3 mt-6">
-                <a
-                  href={`https://twitter.com/share?ref_src=twsrc%5Etfw&text=${tweetText}&url=${animal.url}&hashtags=petsura`}
-                  className="text-3xl bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                  target={`_blank`}
-                >
-                  <FaTwitter />
-                </a>
-
-                <a
-                  href={`mailto:${animal.contact?.email}?subject=${emailSubject}&body=${emailBody}`}
-                  className="text-3xl bg-cyan-500 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded"
-                  target={`_blank`}
-                >
-                  <AiOutlineMail />
-                </a>
-              </div>
-            </div>
-          );
-        })}
-      <button
-        className="w-96 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4"
-        onClick={() =>
-          fetchMore({
-            variables: {
-              offset: data?.feed.length,
-            },
-          })
-        }
-      >
-        <div className="flex items-center justify-evenly text-xl">
+    <>
+      <header className="bg-gray-800 text-white p-4 w-full flex flex-col">
+        <div className="flex justify-center text-lg items-center">
           <FaCat />
-          <span>Get More Fluffs</span>
+          <h1 className="mx-4 text-2xl">Petsura</h1>
           <FaDog />
         </div>
-      </button>
-    </div>
+        <span className="flex justify-center text-sm italic font-extralight items-center">
+          Finding forever homes
+        </span>
+      </header>
+      <div className="flex-col flex items-center mb-4 sm:my-8">
+        {data?.feed
+          .filter((animal) => animal.photos.length > 0)
+          .filter((animal) => animal.contact.email)
+          .filter((animal) => animal.url)
+          .map((animal, i) => {
+            const tweetText = `OMG someone needs to adopt ${animal.name}!`;
+
+            const emailSubject = `I'm interested in adopting ${animal.name}!`;
+            const emailBody = `Hi, I came across ${animal.name} at ${animal.url} and I'm interested in adopting! Looking forward to hearing from you soon!`;
+
+            return (
+              <div
+                key={i}
+                className="flex flex-col items-center w-96 h-auto rounded-md border bg-gray-800 text-white shadow-lg mb-4"
+              >
+                <img
+                  className="w-full h-96 object-cover"
+                  src={animal.photos[0].full}
+                  alt={animal.name}
+                />
+                <h2 className="my-4 text-3xl font-medium">{animal.name}</h2>
+                <div className="flex w-5/6 justify-evenly">
+                  <span className="flex items-center text-sm text-gray-50">
+                    <FaBirthdayCake />
+                    <span className="pl-2">{animal.age?.toLowerCase()}</span>
+                  </span>
+                  <span className="flex items-center text-sm text-gray-50">
+                    <MdQueryStats />
+                    <span className="pl-2">{animal.status?.toLowerCase()}</span>
+                  </span>
+                  <span className="flex items-center text-sm text-gray-50">
+                    <AiOutlineColumnWidth />
+                    <span className="pl-2">{animal.size?.toLowerCase()}</span>
+                  </span>
+                </div>
+                <div className="flex justify-evenly w-5/6 my-8 border-t-2 pt-8">
+                  <a
+                    href={`https://twitter.com/share?ref_src=twsrc%5Etfw&text=${tweetText}&url=${animal.url}&hashtags=petsura`}
+                    className="text-3xl bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    target={`_blank`}
+                  >
+                    <FaTwitter />
+                  </a>
+
+                  <a
+                    href={`mailto:${animal.contact?.email}?subject=${emailSubject}&body=${emailBody}`}
+                    className="text-3xl bg-cyan-500 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded"
+                    target={`_blank`}
+                  >
+                    <AiOutlineMail />
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        <button
+          className="w-96 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4"
+          onClick={() =>
+            fetchMore({
+              variables: {
+                offset: data?.feed.length,
+              },
+            })
+          }
+        >
+          <div className="flex items-center justify-evenly text-xl">
+            <FaCat />
+            <span>Get More Fluffs</span>
+            <FaDog />
+          </div>
+        </button>
+      </div>
+    </>
   );
 };
 
